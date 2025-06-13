29f1ef1c4321679d321d2e248d937be634342b36
zjt <1486805560@qq.com>
2025-06-13 12:23:54 +0800

    1. 端口修改 2. 向量库脚本优化

--- .config/db/vdb.py
+++ .config/db/vdb.py
@@ -1,31 +1,46 @@
-from weaviate.classes.config import Property, DataType, Configure
+# pip install weaviate-client
+
 import weaviate
+from weaviate.collections.classes.config import Property, DataType, Configure
 
-client = weaviate.connect_to_local(
-    host="127.0.0.1",
-    port=8080,
-    grpc_port=50051,
-)
-print(client.is_ready())
 
-client.collections.delete("DocumentInfoByOverlapChunk")
-d2 = client.collections.create(
-    "DocumentInfoByOverlapChunk",
-    properties=[
-        Property(name="documentId", data_type=DataType.INT),
-        Property(name="documentName", data_type=DataType.TEXT),
-        Property(name="documentType", data_type=DataType.TEXT),
-        Property(name="chunkId", data_type=DataType.INT),
-        Property(name="chunkText", data_type=DataType.TEXT),
-    ],
-    vectorizer_config=[
-        Configure.NamedVectors.text2vec_transformers(
-            name="documentNameVector",
-            source_properties=["documentName"]
-        ),
-        Configure.NamedVectors.text2vec_transformers(
-            name="chunkTextVector",
-            source_properties=["chunkText"]
-        ),
-    ],
-)
+def main():
+    global client
+    try:
+        client = weaviate.connect_to_local(
+            host="127.0.0.1",
+            port=8080,
+            grpc_port=50051,
+        )
+        print(client.is_ready())
+
+        client.collections.delete("DocumentInfoByOverlapChunk")
+        d = client.collections.create(
+            "DocumentInfoByOverlapChunk",
+            properties=[
+                Property(name="documentId", data_type=DataType.INT),
+                Property(name="documentName", data_type=DataType.TEXT),
+                Property(name="documentType", data_type=DataType.TEXT),
+                Property(name="chunkId", data_type=DataType.INT),
+                Property(name="chunkText", data_type=DataType.TEXT),
+            ],
+            vectorizer_config=[
+                Configure.NamedVectors.text2vec_transformers(
+                    name="documentNameVector",
+                    source_properties=["documentName"]
+                ),
+                Configure.NamedVectors.text2vec_transformers(
+                    name="chunkTextVector",
+                    source_properties=["chunkText"]
+                ),
+            ],
+        )
+        client.close()
+    except:
+        print("Error")
+        if client:
+            client.close()
+
+
+if __name__ == '__main__':
+    main()
