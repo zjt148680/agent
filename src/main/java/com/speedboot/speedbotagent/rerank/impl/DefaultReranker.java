f5294d92aa1665f1fd9f428afd4a1b6886f56397
zjt <1486805560@qq.com>
2025-06-24 15:51:11 +0800

    rerank：将段落按文档顺序排序

--- src/main/java/com/speedboot/speedbotagent/rerank/impl/DefaultReranker.java
+++ src/main/java/com/speedboot/speedbotagent/rerank/impl/DefaultReranker.java
@@ -1,15 +1,26 @@
 package com.speedboot.speedbotagent.rerank.impl;
 
-import com.speedboot.speedbotagent.dto.knowledge.BaseKnowledgeDTO;
+import com.speedboot.speedbotagent.dto.knowledge.DocumentInfoByOverlapChunkDTO;
 import com.speedboot.speedbotagent.rerank.IReranker;
+import com.speedboot.speedbotagent.weaviate.entity.DocumentInfoByOverlapChunk;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
-public class DefaultReranker implements IReranker {
+public class DefaultReranker implements IReranker<DocumentInfoByOverlapChunkDTO> {
     @Override
-    public List<? extends BaseKnowledgeDTO> rerank(String query, List<? extends BaseKnowledgeDTO> knowledge) {
-        return knowledge;
+    public List<DocumentInfoByOverlapChunkDTO> rerank(String query, List<DocumentInfoByOverlapChunkDTO> knowledge) {
+        List<DocumentInfoByOverlapChunkDTO> res = new ArrayList<>(knowledge);
+        res.sort((k1, k2) -> {
+            DocumentInfoByOverlapChunk d1 = k1.getDocumentInfoByOverlapChunk();
+            DocumentInfoByOverlapChunk d2 = k2.getDocumentInfoByOverlapChunk();
+            if (d1.getDocumentId() == d2.getDocumentId()) {
+                return d1.getChunkId() - d2.getChunkId();
+            }
+            return d1.getDocumentId() - d2.getDocumentId();
+        });
+        return res;
     }
 }
